4f8dab8756439009f4544ab284ffd67efc5e80d9
infinitnet <6189915+infinitnet@users.noreply.github.com>
2024-06-12 20:11:45 +0200

    aider: Initialized Rapid_URL_Indexer_Admin class to set up admin menu and functionality.
    # Aider chat conversation:
    
    USER: There is still no "Rapid URL Indexer" sidebar menu in wp-admin dashboard
    
    ASSISTANT: The issue is likely due to the `Rapid_URL_Indexer_Admin` class not being initialized. We need to call the `init()` method of this class to set up the admin menu and other functionality.
    
    Here's the *SEARCH/REPLACE* block to fix this:
    
    rapid-url-indexer.php
    ```php
    <<<<<<< SEARCH
    add_action('plugins_loaded', array('Rapid_URL_Indexer', 'init'));
    =======
    add_action('plugins_loaded', array('Rapid_URL_Indexer', 'init'));
    Rapid_URL_Indexer_Admin::init();
    >>>>>>> REPLACE
    ```
    
    This change adds a call to `Rapid_URL_Indexer_Admin::init()` after the existing `Rapid_URL_Indexer::init()` call in the main plugin file. This will ensure that the admin class is properly initialized and the admin menu is added.
    
    With this fix, you should see the "Rapid URL Indexer" menu item in the WordPress admin sidebar.

--- rapid-url-indexer.php
+++ rapid-url-indexer.php
@@ -27,6 +27,7 @@ register_activation_hook(__FILE__, array('Rapid_URL_Indexer_Activator', 'activat
 register_deactivation_hook(__FILE__, array('Rapid_URL_Indexer_Deactivator', 'deactivate'));
 
 add_action('plugins_loaded', array('Rapid_URL_Indexer', 'init'));
+Rapid_URL_Indexer_Admin::init();
 
 add_action('init', array('Rapid_URL_Indexer_Customer', 'add_my_account_endpoints'));
 add_action('woocommerce_account_rui-buy-credits_endpoint', array('Rapid_URL_Indexer_Customer', 'add_my_account_endpoint_content'));
