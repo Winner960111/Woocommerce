b064dde50da390b65f67de54a1f46f70e85a3fdb
infinitnet <6189915+infinitnet@users.noreply.github.com>
2024-06-13 15:03:31 +0200

    as

--- templates/customer-buy-credits.php
+++ templates/customer-buy-credits.php
@@ -7,13 +7,11 @@ $products = wc_get_products(array(
 ?>
 
 <?php if (!empty($products)): ?>
+    <h2>Available Packages:</h2>
     <ul class="rui-credits-products">
         <?php foreach ($products as $product): ?>
             <li>
-                <a href="<?php echo esc_url($product->get_permalink()); ?>">
-                    <?php echo esc_html($product->get_name()); ?>
-                </a>
-                <span class="rui-credits-amount">(adds <?php echo esc_html(get_post_meta($product->get_id(), '_credits_amount', true)); ?> credits to your account)</span>
+                <a href="<?php echo esc_url($product->get_permalink()); ?>"><?php echo esc_html($product->get_name()); ?></a><span class="rui-credits-amount"> (adds <?php echo esc_html(get_post_meta($product->get_id(), '_credits_amount', true)); ?> credits to your account)</span>
             </li>
         <?php endforeach; ?>
     </ul>
