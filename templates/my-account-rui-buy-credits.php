36a7891bc77371fe8ac508b049bd7bf5176a98cb
infinitnet <6189915+infinitnet@users.noreply.github.com>
2024-06-13 13:43:19 +0200

    Removed redundant title elements from account pages.

--- templates/my-account-rui-buy-credits.php
+++ templates/my-account-rui-buy-credits.php
@@ -7,10 +7,6 @@ if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly
 }
 
-wc_get_template('myaccount/navigation.php');
-?>
-
 <div class="woocommerce-MyAccount-content">
-    <h1 class="woocommerce-MyAccount-title"><?php esc_html_e('Buy Indexing Credits', 'rapid-url-indexer'); ?></h1>
     <?php include RUI_PLUGIN_DIR . 'templates/customer-buy-credits.php'; ?>
 </div>
--- templates/my-account-rui-projects.php
+++ templates/my-account-rui-projects.php
@@ -7,10 +7,6 @@ if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly
 }
 
-wc_get_template('myaccount/navigation.php');
-?>
-
 <div class="woocommerce-MyAccount-content">
-    <h1 class="woocommerce-MyAccount-title"><?php esc_html_e('My Indexing Projects', 'rapid-url-indexer'); ?></h1>
     <?php include RUI_PLUGIN_DIR . 'templates/customer-projects.php'; ?>
 </div>
