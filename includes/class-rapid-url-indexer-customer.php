81c4834597032eb73893dc808990b79057de0ebc
infinitnet <6189915+infinitnet@users.noreply.github.com>
2024-06-18 15:21:43 +0200

    aider: Changed access level of `submit_project` method to public.

--- includes/class-rapid-url-indexer-customer.php
+++ includes/class-rapid-url-indexer-customer.php
@@ -225,7 +225,7 @@ class Rapid_URL_Indexer_Customer {
         return ob_get_clean();
     }
 
-    private static function submit_project($project_name, $urls, $notify) {
+    public static function submit_project($project_name, $urls, $notify) {
         global $wpdb;
         $user_id = get_current_user_id();
         $table_name = $wpdb->prefix . 'rapid_url_indexer_projects';
