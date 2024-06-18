d94b6b70ea08bf50b36d5cdd4cdeba24cfa8ed89
infinitnet <6189915+infinitnet@users.noreply.github.com>
2024-06-18 09:55:25 +0200

    aider: Added a new method for viewing tasks page in the admin class.

--- includes/class-rapid-url-indexer-admin.php
+++ includes/class-rapid-url-indexer-admin.php
@@ -88,6 +88,8 @@ class Rapid_URL_Indexer_Admin {
 
         include RUI_PLUGIN_DIR . 'templates/admin-projects.php';
     }
+
+    public static function view_tasks_page() {
         $api_key = get_option('rui_speedyindex_api_key');
         $search = isset($_GET['s']) ? sanitize_text_field($_GET['s']) : '';
         $tasks = Rapid_URL_Indexer_API::get_tasks($api_key, 0, $search);
